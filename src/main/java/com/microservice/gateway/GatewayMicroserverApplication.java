32e39012aea5ab87c652429666293605b199bc3e
Mr White <wicho22gonzalez@gmail.com>
2025-11-29 08:31:52 -0600

    Gateway

---
+++ src/main/java/com/microservice/gateway/GatewayMicroserverApplication.java
@@ -0,0 +1,12 @@
+package com.microservice.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMicroserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMicroserverApplication.class, args);
+	}
+}
